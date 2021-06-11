ce0ec53ebdee7b33b54594fda946e3006c9b48d8
Oladapo Ajala <ajalaoladapoemmanuel.ao@gmail.com>
2021-06-12 00:10:00 +0100

    Refactored app.js: commented code and enforced AirBnB linter

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 
+// Error handlers
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+
+// Routers for Tours and Users
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -14,17 +17,23 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// JSON Parser for requests from the client
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+
+/***************************************************/
+// Use this to server static files to the client
+// app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
 //   req.requestTime = new Date().toISOString();
 //   next();
 // });
 
+// Middlewares to mount routers to specific routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Catch all unhandled URLs
 app.all('*', (req, res, next) => {
   next(new AppError(`Invalid URI: ${req.originalUrl} requested!`, 404));
 });
