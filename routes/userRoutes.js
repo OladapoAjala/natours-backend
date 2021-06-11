35a6f253cac5924592f59979400fece504630007
Oladapo Ajala <ajalaoladapoemmanuel.ao@gmail.com>
2021-06-12 00:16:22 +0100

    Refactored userRoutes.js: commented code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,29 @@
 const express = require('express');
-const userController = require('./../controllers/userController');
+const userController = require('../controllers/userController');
+
 const router = express.Router();
 
+/**********
+ * Check if ID is valid before passing on to other API route handlers
+ * **********/
 router.param('id', userController.checkID);
 
+/**********
+ * API router for:
+ *    get all user
+ *    create user
+ * **********/
 router
   .route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
 
+/**********
+ * API router for:
+ *    get user by ID
+ *    update user using ID
+ *    delete user using ID
+ * **********/
 router
   .route('/:id')
   .get(userController.getUser)
