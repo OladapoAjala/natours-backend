9e0e03a407644aa858655893a30f293f4364a374
Oladapo Ajala <ajalaoladapoemmanuel.ao@gmail.com>
2021-06-16 03:06:02 +0100

    Implemented forgot password mail reponse using nodemail

---
+++ utils/email.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: 'Oladapo Ajala <d@drapze.io>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transport.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
