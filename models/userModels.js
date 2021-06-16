2fc911e4b0109d736e0fee04ce4178014595e770
Oladapo Ajala <ajalaoladapoemmanuel.ao@gmail.com>
2021-06-16 13:35:18 +0100

    Implemented passwordChangedAt middleware

--- models/userModels.js
+++ models/userModels.js
@@ -51,6 +51,18 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  /**
+   * This is because DB save operation is slower than JWT generation
+   * Hence, it would appear as tho a password change occured after token
+   * generation.
+   */
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
 userSchema.methods.correctPassword = async function (
   candidatePassword,
   userPassword
@@ -58,6 +70,11 @@ userSchema.methods.correctPassword = async function (
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
+/**
+ * Check if password was changed after token generation.
+ *
+ * If this happens, it renders the token useless
+ */
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   if (this.passwordChangedAt) {
     const changedTimestamp = parseInt(
@@ -78,8 +95,6 @@ userSchema.methods.createPasswordResetToken = function () {
     .createHash('sha256')
     .update(resetToken)
     .digest('hex');
-
-  console.log({ resetToken }, this.passwordResetToken);
 
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
