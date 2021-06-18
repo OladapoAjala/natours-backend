55c249ae0fb68698ff8be32090ecbdb731c12c89
Oladapo Ajala <ajalaoladapoemmanuel.ao@gmail.com>
2021-06-18 23:28:57 +0100

    Implemented user-delete for logged in user, and user update info: name and email

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,15 @@ const User = require('../models/userModels');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+const filterData = (dataObj, ...allowedFields) => {
+  const filteredObj = {};
+  allowedFields.forEach((field) => {
+    filteredObj[field] = dataObj[field];
+  });
+
+  return filteredObj;
+};
+
 exports.checkID = (req, res, next, val) => {
   if (req.params.id * 1 > 9) {
     return res.status(404).json({
@@ -26,6 +35,34 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     data: {
       users,
     },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This url is not for password update', 400));
+  }
+
+  const filteredData = filterData(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredData, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedUser,
+    },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
 
