7d4f9483a0899453ba4d91f794db44ac79f1703d
Oladapo Ajala <ajalaoladapoemmanuel.ao@gmail.com>
2021-08-27 21:05:55 +0100

    protected all routes & restricted certain routes to admin & user

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -9,11 +9,12 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourAndUserIds,
     reviewController.createReview
@@ -24,11 +25,11 @@ router
   .get(reviewController.getReview)
   .patch(
     reviewController.updateReview,
-    authController.restrictTo('admin', 'lead-guide')
+    authController.restrictTo('admin', 'user')
   )
   .delete(
     reviewController.updateReview,
-    authController.restrictTo('admin', 'lead-guide'),
+    authController.restrictTo('admin', 'user'),
     reviewController.deleteReview
   );
 
