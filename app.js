477e7e21a79dad60d4064f4375d0ff310fe436c7
Oladapo Ajala <ajalaoladapoemmanuel.ao@gmail.com>
2021-09-01 22:34:16 +0100

    added pug to for server-side rendering

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const path = require('path');
 
 // Error handlers
 const AppError = require('./utils/appError');
@@ -16,6 +17,13 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+/***************************************************/
+// Use this to serve static files to the client
+app.use(express.static(path.join(__dirname, 'public')));
 
 // MIDDLEWARES
 // console.log(process.env.NODE_ENV);
@@ -56,16 +64,16 @@ app.use(
   })
 );
 
-/***************************************************/
-// Use this to server static files to the client
-// app.use(express.static(`${__dirname}/public`));
-
 // app.use((req, res, next) => {
 //   req.requestTime = new Date().toISOString();
 //   next();
 // });
 
 // Middlewares to mount routers to specific routes
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
