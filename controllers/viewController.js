9567009a79bf3c5f41b5baa14dce7219bb4874d8
Oladapo Ajala <ajalaoladapoemmanuel.ao@gmail.com>
2021-09-03 23:11:29 +0100

    added controllers for rendering overview and tour details

---
+++ controllers/viewController.js
@@ -0,0 +1,25 @@
+const Tour = require('../models/tourModels');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    select: 'review rating user -tour',
+  });
+
+  console.log(tour);
+
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
