847c244b4b9bfaf01b03d7b2f9dcb3fa0f2114e0
Oladapo Ajala <oladapoajala.e@gmail.com>
2021-09-05 23:29:16 +0100

    populated review queries with photo

--- models/reviewModels.js
+++ models/reviewModels.js
@@ -35,7 +35,7 @@ reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
-    select: 'name',
+    select: 'name photo',
   });
 
   // populate({
